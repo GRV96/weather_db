1fbc01dbce28a0a3d17d0f46c74932ed4a50461c
Guyllaume Rousseau <32419088+GRV96@users.noreply.github.com>
2023-04-24 12:52:21 -0400

    Weather data recorded through POST requests (#1)
    * Class WeatherRecord groups weather data.
    * The console displays WeatherRecord instances when a POST request is recieved.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import * as express from "express";
+import { WeatherRecord } from "./weather_record";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (request, response) => {
-	response.send("Hello, world!");
+	response.status(200).send("A weather database");
+});
+
+app.post("/record", (request, response) => {
+	var reqBody = request.body
+	var weatherRecord: WeatherRecord = new WeatherRecord(
+		reqBody.moment,
+		reqBody.temperature,
+		reqBody.precipProb,
+		reqBody.windSpeed);
+	console.log(`${weatherRecord}`);
+	response.sendStatus(200);
 });
 
 console.log("weather_db running.");
---
+++ src/weather_record.ts
@@ -0,0 +1,18 @@
+export class WeatherRecord {
+	readonly moment: Date;
+	readonly temperature: number;
+	readonly precipProb: number;
+	readonly windSpeed: number;
+
+	constructor(moment: Date|string, temperature: number,
+		precipProb: number, windSpeed: number) {
+			this.moment = new Date(moment);
+			this.temperature = temperature;
+			this.precipProb = precipProb;
+			this.windSpeed = windSpeed;
+	}
+
+	toString(): string {
+		return `${this.moment.toISOString()}; ${this.temperature}°C; ${this.precipProb*100}%; ${this.windSpeed} km/h`;
+	}
+}
