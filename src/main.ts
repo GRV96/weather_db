be489e1aa6efbd4cf911dabf55a4a06bbe1a9382
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2023-04-23 23:33:37 -0400

    Response to a POST request

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import * as express from "express";
+import * as bodyParser from 'body-parser';
+import { WeatherRecord } from "weather_record";
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
-	response.send("Hello, world!");
+	response.status(200).send("Hello, world!");
+});
+
+app.post("/record", (request, response) => {
+	console.log(`Request body: \n ${request.body}\nEnd`);
+	response.sendStatus(200);
 });
 
 console.log("weather_db running.");
